8bd5f8a4a7cb841c7395305f3984129598309f9a
FredericEmmanuel <118803449+FredericEmmanuel@users.noreply.github.com>
2025-12-13 20:44:00 +0700

    Update main.py

--- Rate2Pay/Back-end/main.py
+++ Rate2Pay/Back-end/main.py
@@ -1,5 +1,34 @@
-import Login
-import Signup
+import Login_Signup
 import data_acc
 import del_acc
 import RTP
+from Login_Signup import current_user
+
+print("\n=== Rate2Pay ===")
+while True:
+    punya_acc = input("Apakah anda memiliki akun?(Y/N) ").upper()
+    if punya_acc == "Y":
+        Login_Signup.login()
+        break
+    elif punya_acc =="N":
+        Login_Signup.signup()
+        break
+    else:
+        print("Pilihan Invalid!")
+    
+while True:
+    print("\n=== Rate2Pay ===")
+    print("1. RTP")
+    print("2. Data Akun")
+    print("3. Delete Akun")
+    print("4. Keluar")
+    pilihan = int(input("\nApa yang ingin anda lakukan?"))
+    if pilihan == 1:
+        RTP.RTP(current_user)
+    if pilihan == 2:
+        data_acc.data_acc(current_user)
+    if pilihan == 3:
+        del_acc.delete_account(current_user)
+    if pilihan == 4:
+        print("\nTerimaKasih untuk menggunakan Rate2Pay")
+        break
